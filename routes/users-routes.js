a8a558f8af7ab4abdad40a260c535305ecae2687
Sang Nguyen <sangnn2012@gmail.com>
2023-12-14 20:03:38 +0700

    feat: create dummy users' routes

---
+++ routes/users-routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const {
+    getUsers,
+    signup,
+    login,
+} = require('../controllers/users-controllers');
+
+const router = express.Router();
+
+router.get('/', getUsers);
+
+router.post('/signup', signup);
+
+router.post('/login', login);
+
+module.exports = router;
